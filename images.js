892eba1df720aee0609a92af072a22b8b89ccd23
Indiducky <136902267+Indiducky@users.noreply.github.com>
2025-02-09 18:02:49 +1000

    Update images.js

--- images.js
+++ images.js
@@ -1,5 +1,11 @@
 const imageFiles = [
-    "DSCF7466 Edited.JPG",
-    "DSCF7587.JPG",
-    "DSCF7590.JPG"
+2TDXMeY8.jpeg
+DSCF7466 Edited.JPG
+DSCF7587.JPG
+DSCF7590.JPG
+HO3hKUzI.jpeg
+MaxspS5I.jpeg
+R1-02089-0014.JPEG
+iGTjj7wg.jpeg
+oPFnLqOY.jpeg
 ];
